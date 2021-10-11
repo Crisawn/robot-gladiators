bff3536601e8611bacf086ee80cda242f9acadd6
Crisawn <perezisacc1@gmail.com>
2021-10-10 22:15:58 -0500

    console.log

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,9 +6,20 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"] ;
 var enemyHealth = 50;
 var enemyAttack = 12;
+
+console.log(enemyNames);
+console.log(enemyNames.length)
+for(var i = 0; i < 3; i++) {
+    console.log("apple");
+  };
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames[3]);
+
 
 var fight = function() {
     // Alert players that they are starting the round
@@ -58,7 +69,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     else {
       fight();
     }
-  }else {
+  } else {
     window.alert("You need to choose a valid option. Try again!");
   }
 
